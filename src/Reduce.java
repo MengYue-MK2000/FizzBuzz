f0a09e3868f85877102669c68c6c431518da61e9
MengYue-MK2000 <reaganzhang123@gmail.com>
2024-09-10 02:36:39 +0800

    add precondition in reduce and change an small error

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,6 @@
 public class Reduce {
     public static void main(String[] args) {
+        /* assume int num >= 0 */
         int num = 100;
         doReduce(num);
     }
@@ -11,7 +12,7 @@ public class Reduce {
                 num = num / 2;
                 count += 1;
             }
-            else if (num % 2 != 0){
+            else {
                 num = num - 1;
                 count += 1;
             }
