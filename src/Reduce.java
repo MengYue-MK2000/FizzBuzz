23b472448df2093c4330bf5e6ed325d3d85d35c5
MengYue-MK2000 <reaganzhang123@gmail.com>
2024-09-10 00:57:20 +0800

    add multiples

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,17 @@
 public class Reduce {
     public static void main(String[] args) {
-
+        int num = 100;
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                count += 1;
+            }
+            else if (num % 2 != 0){
+                num = num - 1;
+                count += 1;
+            }
+        }
+        System.out.println(count);
     }
 }
